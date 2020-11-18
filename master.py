1c0036b795a5bc97b7298b2e66db37bc398bc96b
Mykhailo Kilianovskyi <kilianovski@gmail.com>
2020-11-18 18:39:50 +0200

    Create inmemory list

--- master.py
+++ master.py
@@ -1,10 +1,22 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class MessageModel(BaseModel):
+    message: str
 
 app = FastAPI()
 
+INMEMORY_MESSAGE_LIST = []
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+
+@app.get("/message")
+def get_message():
+    return INMEMORY_MESSAGE_LIST
+
+@app.post("/message")
+def post_message(message: MessageModel):
+    INMEMORY_MESSAGE_LIST.append(message)
+    return ''
