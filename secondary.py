7007a5e63f8221b6f02920adea7d828320d34497
Mykhailo Kilianovskyi <kilianovski@gmail.com>
2020-11-18 19:07:19 +0200

    Create simple http comm between master and sec

--- secondary.py
+++ secondary.py
@@ -3,27 +3,24 @@ import logging
 from fastapi import FastAPI, Response
 from http import HTTPStatus
 from pydantic import BaseModel
-import requests
 
 logger = logging.getLogger("master")
+
 class MessageModel(BaseModel):
     message: str
-
 
 app = FastAPI()
 
 INMEMORY_MESSAGE_LIST = []
 
 
+
 @app.get("/message")
 def get_message():
     return INMEMORY_MESSAGE_LIST
 
-
-@app.post("/message")
+@app.post("/__message")
 def post_message(message: MessageModel):
-    INMEMORY_MESSAGE_LIST.append(message)
     logging.info(message)
-    r = requests.post('http://127.0.0.1:8889/__message', json={'message': message.message + ' mastered!'})
-    print('r.status_code', r.status_code)
+    INMEMORY_MESSAGE_LIST.append(message)
     return Response(status_code=HTTPStatus.NO_CONTENT.value)
