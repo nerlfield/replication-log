500ca4b281229d7f7aa526ec1c5320a76b195e73
Danylo Kovalenko <danylo.kovalenko@jet.computer>
2020-12-26 18:43:27 +0200

    Added retry policy with error simulation example.

--- master.py
+++ master.py
@@ -4,14 +4,20 @@ import logging
 from fastapi import FastAPI, Response
 from http import HTTPStatus
 from pydantic import BaseModel
+import time
 import grequests
 from threading import Thread
+from requests import Session
+import gevent
+from gevent import monkey as curious_george
+from gevent.pool import Pool
 
 logger = logging.getLogger("master")
 
 
 class MessageModel(BaseModel):
     message: str
+    is_blocked: bool = False
 
 
 app = FastAPI(debug=True)
@@ -20,15 +26,40 @@ app = FastAPI(debug=True)
 INMEMORY_MESSAGE_LIST = ["test"]
 SECONDARIES = [f"http://{sec_name}:8000/__message" for sec_name in os.environ['SECONDARIES_NAMES'].split(sep=',')]
 
+def imap(requests, stream=False, size=2, exception_handler=None):
+    pool = Pool(size)
 
-@app.get("/message")
-def get_message():
-    return INMEMORY_MESSAGE_LIST
+    def send(r):
+        return r.send(stream=stream)
+
+    for request in pool.imap_unordered(send, requests):
+        if request.response.status_code == 500:
+            ex_result = exception_handler(request, stream)
+            if ex_result is not None:
+                yield ex_result
+            continue
+
+        if request.response is not None:
+            yield request.response
+            
+
+    pool.join()
+
+def exception_handler(r, stream=False):
+    request = r.send(stream=stream)
+    retr_timeout = 3
+    while request.response.status_code == 500:
+        time.sleep(retr_timeout)
+        retr_timeout = retr_timeout**2
+        print(f'Retry func with timeout: {retr_timeout}')
+        request = r.send(stream=stream)
+    
+    return request.response
 
 
 def replicate_to_secondaries(message, write_concern):
-    rs = [grequests.post(secondary, json={'message': message.message}) for secondary in SECONDARIES]
-    results = grequests.imap(rs)
+    rs = [grequests.post(secondary, json={'message': message.message, 'is_blocked': message.is_blocked}) for secondary in SECONDARIES]
+    results = imap(rs, exception_handler=exception_handler)
 
     return results
 
@@ -36,7 +67,6 @@ def replicate_to_secondaries(message, write_concern):
 def is_success_replication(results, write_concern):
 
     for i in range(0, write_concern - 1):
-
         if not next(results):
             return False
     return True
@@ -45,6 +75,10 @@ def threading_start(results, write_concern):
     threadOfUpdates = Thread(target=is_success_replication, args=[results, 3])
     threadOfUpdates.start()
 
+@app.get("/message")
+def get_message():
+    return INMEMORY_MESSAGE_LIST
+
 @app.post("/message")
 def post_message(message: MessageModel, write_concern: int):
     INMEMORY_MESSAGE_LIST.append(message)
@@ -52,7 +86,6 @@ def post_message(message: MessageModel, write_concern: int):
     print(f"With write concern: {write_concern}")
 
     replication_results = replicate_to_secondaries(message, write_concern)
-
 
     if is_success_replication(replication_results, write_concern):
         threading_start(replication_results, write_concern)
--- secondary.py
+++ secondary.py
@@ -2,7 +2,7 @@ import os
 import time
 from typing import Optional
 import logging
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 from http import HTTPStatus
 from pydantic import BaseModel
 
@@ -10,11 +10,23 @@ logger = logging.getLogger("master")
 
 class MessageModel(BaseModel):
     message: str
+    is_blocked: bool
 
 app = FastAPI(debug=True)
 
 INMEMORY_MESSAGE_LIST = []
 DELAY = None if 'DELAY' not in os.environ else float(os.environ['DELAY'])
+
+
+ERROR_K = 0 if 'ERROR_K' not in os.environ else float(os.environ['ERROR_K'])
+
+def error_simulation(is_blocked):
+    global ERROR_K
+    if ERROR_K > 0 and is_blocked:
+        ERROR_K -= 1
+        msg = f'Okay, we got an error, retry your request pls. You will get {ERROR_K} more errors'
+        logging.info(msg)
+        raise Exception(msg)
 
 
 @app.get("/message")
@@ -26,11 +38,17 @@ def get_message():
 def post_message(message: MessageModel):
     logging.info(message)
 
-    if DELAY is not None and DELAY > 0:
-        print(f'Introducing delay {DELAY} sec..')
-        time.sleep(DELAY)
-        print(f'Delay completed!')
+    try:
+        error_simulation(message.is_blocked)
+        if DELAY is not None and DELAY > 0:
+            print(f'Introducing delay {DELAY} sec..')
+            time.sleep(DELAY)
+            print(f'Delay completed!')
 
-    INMEMORY_MESSAGE_LIST.append(message)
-    print(INMEMORY_MESSAGE_LIST)
-    return Response(status_code=HTTPStatus.NO_CONTENT.value)
+        INMEMORY_MESSAGE_LIST.append(message)
+        print(INMEMORY_MESSAGE_LIST)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
+    except:
+        raise HTTPException(status_code=500, detail="Item not found")
+        
+    
